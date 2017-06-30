583aa1db7b50b472ef261fc13f6b7733c9f679d8
Anılcan ÇAKIR <anilcan@kodizm.com>
2017-06-30 11:30:41 +0300

    Before the clean.

---
+++ resources/assets/js/components/inputs/vInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="field">
+        <label v-if="label" :for="id" class="label">{{ label }}</label>
+        <p class="control">
+            <input :type="type" :name="name" :id="id" class="input">
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            id: {
+                type: String,
+                required: name
+            },
+            label: {
+                type: String,
+                default: null
+            },
+            type: {
+                type: String,
+                default: 'text'
+            }
+        }
+    }
+</script>
---
+++ resources/assets/js/components/vForm.vue
@@ -0,0 +1,20 @@
+<template>
+    <form action="{{ action }}" method="{{ method }}">
+        <slot></slot>
+    </form>
+</template>
+
+<script>
+    export default {
+        props: {
+            action: {
+                type: String,
+                required: true
+            },
+            method: {
+                type: String,
+                default: 'POST'
+            }
+        }
+    }
+</script>
